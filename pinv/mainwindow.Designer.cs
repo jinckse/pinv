06eadb5d7d5f8e8f0b3f05bb197f4f83f1ee14cf
jinckse <jnix88@gmail.com>
2017-04-26 16:47:48 -0500

    Reorg: added buttons for using database

--- pinv/mainwindow.Designer.cs
+++ pinv/mainwindow.Designer.cs
@@ -32,7 +32,6 @@
             this.statusStrip1 = new System.Windows.Forms.StatusStrip();
             this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
             this.toolStripStatusLabel2 = new System.Windows.Forms.ToolStripStatusLabel();
-            this.outputPane = new System.Windows.Forms.RichTextBox();
             this.panel1 = new System.Windows.Forms.Panel();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.submitOutput = new System.Windows.Forms.Button();
@@ -49,6 +48,15 @@
             this.dbIPTextBox = new System.Windows.Forms.TextBox();
             this.dbUNameTextBox = new System.Windows.Forms.TextBox();
             this.dbPassTextBox = new System.Windows.Forms.MaskedTextBox();
+            this.connectButton = new System.Windows.Forms.Button();
+            this.disconnectButton = new System.Windows.Forms.Button();
+            this.outputPane = new System.Windows.Forms.RichTextBox();
+            this.activityPaneLabel = new System.Windows.Forms.Label();
+            this.locateItemButton = new System.Windows.Forms.Button();
+            this.viewItemButton = new System.Windows.Forms.Button();
+            this.addItemButton = new System.Windows.Forms.Button();
+            this.updateItemButton = new System.Windows.Forms.Button();
+            this.deleteItemButton = new System.Windows.Forms.Button();
             this.statusStrip1.SuspendLayout();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.queriesBindingSource)).BeginInit();
@@ -82,30 +90,17 @@
             this.toolStripStatusLabel2.Size = new System.Drawing.Size(178, 20);
             this.toolStripStatusLabel2.Text = "MySQL Connection Status";
             // 
-            // outputPane
-            // 
-            this.outputPane.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.outputPane.BackColor = System.Drawing.SystemColors.WindowText;
-            this.outputPane.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.outputPane.Font = new System.Drawing.Font("Courier New", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.outputPane.ForeColor = System.Drawing.Color.Lime;
-            this.outputPane.Location = new System.Drawing.Point(0, 93);
-            this.outputPane.Name = "outputPane";
-            this.outputPane.ReadOnly = true;
-            this.outputPane.Size = new System.Drawing.Size(1259, 387);
-            this.outputPane.TabIndex = 0;
-            this.outputPane.Text = "Welcome to the PINV system";
-            this.outputPane.UseWaitCursor = true;
-            this.outputPane.WordWrap = false;
-            this.outputPane.ZoomFactor = 1.2F;
-            this.outputPane.TextChanged += new System.EventHandler(this.outputPane_TextChanged);
-            // 
             // panel1
             // 
             this.panel1.AutoScroll = true;
             this.panel1.AutoSize = true;
             this.panel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.panel1.Controls.Add(this.deleteItemButton);
+            this.panel1.Controls.Add(this.updateItemButton);
+            this.panel1.Controls.Add(this.addItemButton);
+            this.panel1.Controls.Add(this.viewItemButton);
+            this.panel1.Controls.Add(this.locateItemButton);
+            this.panel1.Controls.Add(this.activityPaneLabel);
             this.panel1.Controls.Add(this.comboBox1);
             this.panel1.Controls.Add(this.submitOutput);
             this.panel1.Controls.Add(this.outputPane);
@@ -127,20 +122,20 @@
             "Q4: Where can I find a 3 foot USB Type-C cable?",
             "Q5: Where have I stored the elast expensive capacitors I own?",
             "Q6: How many resistors do I have stored in the system?"});
-            this.comboBox1.Location = new System.Drawing.Point(137, 65);
+            this.comboBox1.Location = new System.Drawing.Point(368, 45);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(814, 24);
+            this.comboBox1.Size = new System.Drawing.Size(855, 24);
             this.comboBox1.TabIndex = 2;
-            this.comboBox1.Text = "Select a Query";
+            this.comboBox1.Text = "Select a Sample Query";
             this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
             // submitOutput
             // 
-            this.submitOutput.Location = new System.Drawing.Point(6, 65);
+            this.submitOutput.Location = new System.Drawing.Point(209, 45);
             this.submitOutput.Name = "submitOutput";
-            this.submitOutput.Size = new System.Drawing.Size(125, 23);
+            this.submitOutput.Size = new System.Drawing.Size(153, 23);
             this.submitOutput.TabIndex = 1;
-            this.submitOutput.Text = "Submit Query";
+            this.submitOutput.Text = "Submit Sample Query";
             this.submitOutput.UseVisualStyleBackColor = true;
             this.submitOutput.Click += new System.EventHandler(this.submitOutput_Click);
             // 
@@ -163,10 +158,10 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Impact", 80F);
-            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Font = new System.Drawing.Font("Impact", 90F);
+            this.label1.Location = new System.Drawing.Point(1, 23);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(318, 162);
+            this.label1.Size = new System.Drawing.Size(355, 183);
             this.label1.TabIndex = 3;
             this.label1.Text = "PINV";
             // 
@@ -252,12 +247,128 @@
             this.dbPassTextBox.UseSystemPasswordChar = true;
             this.dbPassTextBox.MaskInputRejected += new System.Windows.Forms.MaskInputRejectedEventHandler(this.dbPassTextBox_MaskInputRejected);
             // 
+            // connectButton
+            // 
+            this.connectButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.connectButton.Font = new System.Drawing.Font("Impact", 20F);
+            this.connectButton.ForeColor = System.Drawing.Color.White;
+            this.connectButton.Location = new System.Drawing.Point(900, 40);
+            this.connectButton.Name = "connectButton";
+            this.connectButton.Size = new System.Drawing.Size(323, 60);
+            this.connectButton.TabIndex = 11;
+            this.connectButton.Text = "CONNECT";
+            this.connectButton.UseVisualStyleBackColor = false;
+            // 
+            // disconnectButton
+            // 
+            this.disconnectButton.BackColor = System.Drawing.Color.Red;
+            this.disconnectButton.Font = new System.Drawing.Font("Impact", 20F);
+            this.disconnectButton.ForeColor = System.Drawing.Color.White;
+            this.disconnectButton.Location = new System.Drawing.Point(900, 125);
+            this.disconnectButton.Name = "disconnectButton";
+            this.disconnectButton.Size = new System.Drawing.Size(323, 60);
+            this.disconnectButton.TabIndex = 12;
+            this.disconnectButton.Text = "DISCONNECT";
+            this.disconnectButton.UseVisualStyleBackColor = false;
+            // 
+            // outputPane
+            // 
+            this.outputPane.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.outputPane.BackColor = System.Drawing.SystemColors.WindowText;
+            this.outputPane.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.outputPane.Font = new System.Drawing.Font("Courier New", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputPane.ForeColor = System.Drawing.Color.Lime;
+            this.outputPane.Location = new System.Drawing.Point(32, 106);
+            this.outputPane.Name = "outputPane";
+            this.outputPane.ReadOnly = true;
+            this.outputPane.Size = new System.Drawing.Size(480, 347);
+            this.outputPane.TabIndex = 0;
+            this.outputPane.Text = "Welcome to the PINV system";
+            this.outputPane.UseWaitCursor = true;
+            this.outputPane.WordWrap = false;
+            this.outputPane.ZoomFactor = 1.2F;
+            this.outputPane.TextChanged += new System.EventHandler(this.outputPane_TextChanged);
+            // 
+            // activityPaneLabel
+            // 
+            this.activityPaneLabel.AutoSize = true;
+            this.activityPaneLabel.Font = new System.Drawing.Font("Impact", 12F);
+            this.activityPaneLabel.ForeColor = System.Drawing.Color.Snow;
+            this.activityPaneLabel.Location = new System.Drawing.Point(31, 83);
+            this.activityPaneLabel.Name = "activityPaneLabel";
+            this.activityPaneLabel.Size = new System.Drawing.Size(116, 25);
+            this.activityPaneLabel.TabIndex = 3;
+            this.activityPaneLabel.Text = "Activity Pane";
+            // 
+            // locateItemButton
+            // 
+            this.locateItemButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.locateItemButton.Font = new System.Drawing.Font("Impact", 12F);
+            this.locateItemButton.ForeColor = System.Drawing.Color.Snow;
+            this.locateItemButton.Location = new System.Drawing.Point(521, 106);
+            this.locateItemButton.Name = "locateItemButton";
+            this.locateItemButton.Size = new System.Drawing.Size(135, 35);
+            this.locateItemButton.TabIndex = 9;
+            this.locateItemButton.Text = "Locate Item";
+            this.locateItemButton.UseVisualStyleBackColor = false;
+            // 
+            // viewItemButton
+            // 
+            this.viewItemButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.viewItemButton.Font = new System.Drawing.Font("Impact", 12F);
+            this.viewItemButton.ForeColor = System.Drawing.Color.Snow;
+            this.viewItemButton.Location = new System.Drawing.Point(662, 106);
+            this.viewItemButton.Name = "viewItemButton";
+            this.viewItemButton.Size = new System.Drawing.Size(135, 35);
+            this.viewItemButton.TabIndex = 10;
+            this.viewItemButton.Text = "View Item";
+            this.viewItemButton.UseVisualStyleBackColor = false;
+            // 
+            // addItemButton
+            // 
+            this.addItemButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.addItemButton.Font = new System.Drawing.Font("Impact", 12F);
+            this.addItemButton.ForeColor = System.Drawing.Color.Snow;
+            this.addItemButton.Location = new System.Drawing.Point(803, 106);
+            this.addItemButton.Name = "addItemButton";
+            this.addItemButton.Size = new System.Drawing.Size(135, 35);
+            this.addItemButton.TabIndex = 11;
+            this.addItemButton.Text = "Add Item";
+            this.addItemButton.UseVisualStyleBackColor = false;
+            // 
+            // updateItemButton
+            // 
+            this.updateItemButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.updateItemButton.Font = new System.Drawing.Font("Impact", 12F);
+            this.updateItemButton.ForeColor = System.Drawing.Color.Snow;
+            this.updateItemButton.Location = new System.Drawing.Point(944, 106);
+            this.updateItemButton.Name = "updateItemButton";
+            this.updateItemButton.Size = new System.Drawing.Size(135, 35);
+            this.updateItemButton.TabIndex = 12;
+            this.updateItemButton.Text = "Update Item";
+            this.updateItemButton.UseVisualStyleBackColor = false;
+            // 
+            // deleteItemButton
+            // 
+            this.deleteItemButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.deleteItemButton.Font = new System.Drawing.Font("Impact", 12F);
+            this.deleteItemButton.ForeColor = System.Drawing.Color.Snow;
+            this.deleteItemButton.Location = new System.Drawing.Point(1085, 106);
+            this.deleteItemButton.Name = "deleteItemButton";
+            this.deleteItemButton.Size = new System.Drawing.Size(135, 35);
+            this.deleteItemButton.TabIndex = 13;
+            this.deleteItemButton.Text = "Delete Item";
+            this.deleteItemButton.UseVisualStyleBackColor = false;
+            // 
             // mainwindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
             this.ClientSize = new System.Drawing.Size(1262, 673);
+            this.Controls.Add(this.disconnectButton);
+            this.Controls.Add(this.connectButton);
             this.Controls.Add(this.dbPassTextBox);
             this.Controls.Add(this.dbUNameTextBox);
             this.Controls.Add(this.dbIPTextBox);
@@ -269,12 +380,14 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.statusStrip1);
+            this.MaximizeBox = false;
             this.Name = "mainwindow";
             this.Text = "PINV - Personal Inventory System";
             this.Load += new System.EventHandler(this.mainwindow_Load);
             this.statusStrip1.ResumeLayout(false);
             this.statusStrip1.PerformLayout();
             this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.queriesBindingSource)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.mainwindowBindingSource)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.queriesBindingSource1)).EndInit();
@@ -288,7 +401,6 @@
         private System.Windows.Forms.StatusStrip statusStrip1;
         private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
         private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel2;
-        private System.Windows.Forms.RichTextBox outputPane;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Button submitOutput;
         private System.Windows.Forms.BindingSource queriesBindingSource;
@@ -305,6 +417,15 @@
         private System.Windows.Forms.TextBox dbNameTextBox;
         private System.Windows.Forms.TextBox dbIPTextBox;
         private System.Windows.Forms.TextBox dbUNameTextBox;
+        private System.Windows.Forms.Button connectButton;
+        private System.Windows.Forms.Button disconnectButton;
+        private System.Windows.Forms.Label activityPaneLabel;
+        private System.Windows.Forms.RichTextBox outputPane;
+        private System.Windows.Forms.Button deleteItemButton;
+        private System.Windows.Forms.Button updateItemButton;
+        private System.Windows.Forms.Button addItemButton;
+        private System.Windows.Forms.Button viewItemButton;
+        private System.Windows.Forms.Button locateItemButton;
     }
 }
 
